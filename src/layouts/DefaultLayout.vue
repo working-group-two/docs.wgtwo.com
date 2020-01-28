b67928accfb14059e5c4c4fdd873e23e3585b078
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2020-01-28 14:00:54 +0100

    Fix layout in tablet size

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -7,17 +7,20 @@
       role="alert"
     >These APIs and corresponding docs are under development and may change without notice</b-notification>
     <main class="section main">
-      <div class="columns">
+      <div class="columns is-hidden-touch">
         <article class="column is-three-fifths">
           <slot></slot>
         </article>
-        <nav class="column articlenav is-one-fifths is-hidden-touch">
+        <nav class="column articlenav is-one-fifths">
           <slot name="articlenav"></slot>
         </nav>
-        <nav class="column docsnav is-one-fifths is-hidden-touch">
+        <nav class="column docsnav is-one-fifths">
           <slot name="docsnav"></slot>
         </nav>
       </div>
+      <article class="is-hidden-desktop">
+        <slot></slot>
+      </article>
     </main>
     <footer class="footer is-hidden-desktop" v-if="this.$slots.docsnav">
       <slot name="docsnav"></slot>
