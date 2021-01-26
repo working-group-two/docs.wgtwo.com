c10cfbb179f90558c72a4e731cd19ebb03ddaf11
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2021-01-26 13:08:04 +0100

    Add examples/kotlin/thirdpartydev/events/

---
+++ examples/kotlin/thirdpartydev/events/src/main/kotlin/GetEvents.kt
@@ -0,0 +1,35 @@
+import com.wgtwo.api.common.Environment
+import com.wgtwo.api.events.v0.EventsProto
+import com.wgtwo.api.events.v0.EventsServiceGrpc
+import com.wgtwo.api.util.auth.Clients
+import com.wgtwo.api.util.auth.UserToken
+import io.grpc.stub.StreamObserver
+
+private val channel = Clients.createChannel(Environment.PROD)
+private val credentials = UserToken("ACCESS_TOKEN") // Add your credentials
+private val stub = EventsServiceGrpc.newStub(channel).withCallCredentials(credentials)
+
+fun main() {
+    val request = EventsProto.SubscribeEventsRequest.newBuilder()
+        .addType(EventsProto.EventType.VOICE_EVENT)
+        .addType(EventsProto.EventType.VOICEMAIL_EVENT)
+        .build()
+    stub.subscribe(request, object : StreamObserver<EventsProto.SubscribeEventsResponse> {
+        override fun onNext(response: EventsProto.SubscribeEventsResponse) {
+            response.eventList.forEach {
+                    event -> println("Received event of type: ${event.eventCase}")
+            }
+        }
+
+        override fun onError(throwable: Throwable) {
+            println("Got error: ${throwable.message}")
+            // TODO: Reconnect
+        }
+
+        override fun onCompleted() {
+            println("Connection closed by the server")
+        }
+    })
+    // Wait for stream to close
+    try { Thread.currentThread().join() } catch (e: InterruptedException) {}
+}
