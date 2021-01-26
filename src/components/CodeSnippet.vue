808e504f5167a6f759f6e8702c12e8e381fb32b2
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2021-01-26 12:55:20 +0100

    Wrap CodeSnippet content in ClientOnly

--- src/components/CodeSnippet.vue
+++ src/components/CodeSnippet.vue
@@ -1,54 +1,56 @@
 <template>
-  <language-tabs :kotlinDeps="kotlinDeps">
-    <template #grpcurl v-if="grpcurlOperator && isOperatorTabSelected || grpcurlThirdpartydev && isThirdPartyDeveloperTabSelected">
-      <GithubCode
-        :fileUrl="grpcurlOperator"
-        language="shell"
-        v-if="isOperatorTabSelected"
-        key="operator"
-        @code-fetched="$forceUpdate()"
-      />
-      <GithubCode
-        :fileUrl="grpcurlThirdpartydev"
-        language="shell"
-        v-if="isThirdPartyDeveloperTabSelected"
-        key="thirdpartydev"
-        @code-fetched="$forceUpdate()"
-      />
-    </template>
-    <template #curl v-if="curlOperator && isOperatorTabSelected || curlThirdpartydev && isThirdPartyDeveloperTabSelected">
-      <GithubCode
-        :fileUrl="curlOperator"
-        language="shell"
-        v-if="isOperatorTabSelected"
-        key="operator"
-        @code-fetched="$forceUpdate()"
-      />
-      <GithubCode
-        :fileUrl="curlThirdpartydev"
-        language="shell"
-        v-if="isThirdPartyDeveloperTabSelected"
-        key="thirdpartydev"
-        @code-fetched="$forceUpdate()"
-      />
-    </template>
-    <template #kotlin v-if="kotlinOperator && isOperatorTabSelected || kotlinThirdpartydev && isThirdPartyDeveloperTabSelected">
-      <GithubCode
-        :fileUrl="kotlinOperator"
-        language="kotlin"
-        v-if="isOperatorTabSelected"
-        key="operator"
-        @code-fetched="$forceUpdate()"
-      />
-      <GithubCode
-        :fileUrl="kotlinThirdpartydev"
-        language="kotlin"
-        v-if="isThirdPartyDeveloperTabSelected"
-        key="thirdpartydev"
-        @code-fetched="$forceUpdate()"
-      />
-    </template>
-  </language-tabs>
+  <ClientOnly>
+    <language-tabs :kotlinDeps="kotlinDeps">
+      <template #grpcurl v-if="grpcurlOperator && isOperatorTabSelected || grpcurlThirdpartydev && isThirdPartyDeveloperTabSelected">
+        <GithubCode
+          :fileUrl="grpcurlOperator"
+          language="shell"
+          v-if="isOperatorTabSelected"
+          key="operator"
+          @code-fetched="$forceUpdate()"
+        />
+        <GithubCode
+          :fileUrl="grpcurlThirdpartydev"
+          language="shell"
+          v-if="isThirdPartyDeveloperTabSelected"
+          key="thirdpartydev"
+          @code-fetched="$forceUpdate()"
+        />
+      </template>
+      <template #curl v-if="curlOperator && isOperatorTabSelected || curlThirdpartydev && isThirdPartyDeveloperTabSelected">
+        <GithubCode
+          :fileUrl="curlOperator"
+          language="shell"
+          v-if="isOperatorTabSelected"
+          key="operator"
+          @code-fetched="$forceUpdate()"
+        />
+        <GithubCode
+          :fileUrl="curlThirdpartydev"
+          language="shell"
+          v-if="isThirdPartyDeveloperTabSelected"
+          key="thirdpartydev"
+          @code-fetched="$forceUpdate()"
+        />
+      </template>
+      <template #kotlin v-if="kotlinOperator && isOperatorTabSelected || kotlinThirdpartydev && isThirdPartyDeveloperTabSelected">
+        <GithubCode
+          :fileUrl="kotlinOperator"
+          language="kotlin"
+          v-if="isOperatorTabSelected"
+          key="operator"
+          @code-fetched="$forceUpdate()"
+        />
+        <GithubCode
+          :fileUrl="kotlinThirdpartydev"
+          language="kotlin"
+          v-if="isThirdPartyDeveloperTabSelected"
+          key="thirdpartydev"
+          @code-fetched="$forceUpdate()"
+        />
+      </template>
+    </language-tabs>
+  </ClientOnly>
 </template>
 <script>
 import { mapGetters } from "vuex";
